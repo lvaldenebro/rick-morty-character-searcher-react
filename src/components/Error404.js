5395545fb76535859fb896ddf35b7d882ec6cf47
lvaldenebro <lauravaldeal@gmail.com>
2022-12-14 15:10:36 +0100

    FINAL PSUH: adds a return link from 404 error

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,5 +1,7 @@
 //Resources
 import error from '../images/error-404.jpg';
+//Router
+import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return (
@@ -7,6 +9,7 @@ const Error404 = () => {
             <h1>ERROR 404</h1>
             <p>The website you are looking for does not exist.</p>
             <img className='img' src={error} alt="Rick&Morty error 404" />
+            <Link className='main_return' to='/'>Volver {'>'}</Link>
         </>
 )};
 export default Error404;
