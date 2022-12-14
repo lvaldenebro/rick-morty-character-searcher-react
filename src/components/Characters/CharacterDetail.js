ea78b0a051bfe82225c49237e44fe302c7a14e83
lvaldenebro <lauravaldeal@gmail.com>
2022-12-14 09:30:13 +0100

    Checks which is the status and the species of the character and emoji to them

--- src/components/Characters/CharacterDetail.js
+++ src/components/Characters/CharacterDetail.js
@@ -4,16 +4,49 @@ import { Link } from 'react-router-dom';
 import '../../styles/components/CharacterDetail.scss';
 
 const CharacterDetail = ( {character} ) => {
+
+    const isDead = (status) => {
+        if (status === 'Dead') {
+        return '💀'
+        }
+        switch (status) {
+            case 'Dead':
+            return '💀';
+            break;
+            case 'unknown':
+            return '❔';
+            break;
+            case 'Alive':
+            return '🥳';
+            break
+            default:
+            return status;
+        }
+};
+
+    const whichSpecies = (species) => {
+        switch (species) {
+            case 'Human':
+            return '👤';
+            break;
+            case 'Alien':
+            return '👽';
+            break;
+            default:
+            return species;
+        }
+    };
+
     return (
         <>
             <Link className='main_return' to='/'>Volver {'>'}</Link>
             <article key={character.id} className='main_item'>
-                {/* link/navlink */}
                 <img src={character.image} alt={character.name} title={`Image of ${character.name}`}/>
                 <section>
                     <h4>{character.name}</h4> {/* name+surname */}
-                    <p>{`Status: ${character.status}`}</p>
-                    <p>{`Species: ${character.species}`}</p>
+                    {/* call the function */}
+                    <p>{`Status: ${character.status}${isDead(character.status)}`}</p>
+                    <p>{`Species: ${character.species}${whichSpecies(character.species)}`}</p>
                     <p>{`Origin: ${character.planet}`}</p>
                     <p>{`Episodes: ${character.episodes}`}</p>
                 </section>
